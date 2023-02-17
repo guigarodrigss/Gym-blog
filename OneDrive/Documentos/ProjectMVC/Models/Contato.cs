91ae0d80e508db46e92b19f97659326a3785dc28
Guilherme Rodrigues <guilherme-rodrigues75@hotmail.com>
2023-02-17 14:11:35 -0300

    Add existing file

---
+++ OneDrive/Documentos/ProjectMVC/Models/Contato.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectMVC.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public bool Ativo { get; set; }
+
+        
+    }
+}
---
+++ OneDrive/Documentos/ProjectMVC/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+var slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("carousel-item");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.opacity = "0";
+  }
+  slides[slideIndex-1].style.opacity = "1";
+}
+
+showSlides(slideIndex);
